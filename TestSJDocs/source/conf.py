20767fcbfd5176471da739e7175217c7bd4c1524
MaslovaEV <maslova_ev@bw-sw.com>
2017-07-26 16:15:19 +0700

    Update conf.py

--- TestSJDocs/source/conf.py
+++ TestSJDocs/source/conf.py
@@ -206,12 +206,6 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'SJdoc'
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
-        ],
-    }
-
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
